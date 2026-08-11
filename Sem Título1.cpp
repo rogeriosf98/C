cd22eab4231f25d5247158f9f0ea93824d80e127
Rogério Ferreira <rogeriosfpro@gmail.com>
2026-08-10 21:24:43 -0300

    Initial commit

---
+++ Sem Título1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int numero = 10;
+	float numeroQuebrado = 50.48;
+	char letra = 'A';
+	char nome[10] = "Rogerinho";
+	
+	printf("numero: %d \n", numero);
+	printf("Flutuante: %.2f \n", numeroQuebrado);
+	printf("Letra: %c \n", letra);
+	printf("Nome: %s", nome);
+	
+	return 0;
+}
