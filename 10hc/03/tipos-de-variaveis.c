b3d1971b7ca405014faa647a420492ed9c5ea2d4
Rogério Ferreira <rogeriosfpro@gmail.com>
2026-08-15 21:18:21 -0300

    tipos de variaveis em c

---
+++ 10hc/03/tipos-de-variaveis.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main () {
+    //caracteres
+    char nome_da_variavel = 'a'; // -127 127
+    unsigned char variavel4; // 0 255 
+    
+    //numeros inteiros
+    short int nome_da_variavel2;
+    int nome_da_variavel3;
+    long int nome_da_variavel;
+
+    unsigned short int variavel1;
+    unsigned int variavel2;
+    unsigned long int variavel3;
+
+    // numeros reais
+    float nome_da_variavel5;
+    double nome_da_variavel6;
+    long double nome_da_variavel7;
+
+    //constante nomeada
+    const int max = 100;
+}
--- 10hc/main.c
+++ 10hc/main.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
 void main () {
-    int a = 1;
-    int b = 2;
-    int c = a + b;
-    printf("%d", c);
+
 }
