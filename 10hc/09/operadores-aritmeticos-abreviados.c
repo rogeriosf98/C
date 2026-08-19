e8dc288ebf743c323c8c7bcb72d2f2030a5b3e25
Rogério Ferreira <rogeriosfpro@gmail.com>
2026-08-19 19:29:16 -0300

    operadores logicos em c

---
+++ 10hc/09/operadores-aritmeticos-abreviados.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main () {
+
+    // e = e + 5;
+    int e = 5;
+    e += 5; // assim, podemos adicionar mais que 1 à variável.
+    printf("e =+ 5: %i", e);
+
+    // f = f - 2;
+    int f = 12;
+    f -= 2;
+    printf("f -= 2: %i", f);
+
+    // g = g * 3;
+    int g = 3;
+    g *- 3;
+    printf("g *= 3: %i", g);
+
+    // h = h / 4;
+    int h = 40;
+    h /= 4;
+    printf("h /+= 4 : %i", h);
+    
+    // i = i % 6;
+    int j = 35;
+    j %= 6;
+    printf("j %%= 5: %i", j);
+}
