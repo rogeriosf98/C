569690278e8abb982f64c6129061188193501835
Rogério Ferreira <rogeriosfpro@gmail.com>
2026-08-20 11:08:43 -0300

    estrutura condicional aninhada

--- 10hc/13/operador-ternario.c
+++ 10hc/13/operador-ternario.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
+/*Como ideia do projeto, vamos verificar se o numero é positivo, negativo, ou = 0.*/
+
 void main () {
-    int x, y, maior, menor;
-    printf("Digite dois numeros inteiros: ");
-    scanf(" %d %d", &x, &y); 
-
-    // maior = x > y ? x : y;
-    if (x > y) {
-        maior = x;
-        printf(""); // se voce tiver um print, alme de uma condição
-        maior = y;
-        menor = x;  // se tiver duas condições que vao para redundancia.
-    }               // o operador ternário n~çao consegue te entregar o resultado esperado.
-
-    printf("Maior: %d", maior);
+    int x;
+    printf("Digite um número: ");
+    scanf("%i", x);
+    if (x < 0) {
+        printf(" %i e negativo", x);
+    } else {
+        if (x == 0) {
+            printf("%i e zero!", x);
+        } else {
+            printf("%i e positivo", x);
+        }
+    }
 }
